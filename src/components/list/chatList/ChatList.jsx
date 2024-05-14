934276c61039e81fbbfd25604a7a988067873d68
MMcUni <mmccur300@caledonian.ac.uk>
2024-05-14 17:53:41 +0100

    profile avatar area formatting set

--- src/components/list/chatList/ChatList.jsx
+++ src/components/list/chatList/ChatList.jsx
@@ -1,9 +1,17 @@
-import './chatList.css'
+import './chatList.css';
 
 const ChatList = () => {
   return (
-    <div className='chatList'>ChatList</div>
-  )
+    <div className='chatList'>
+      <div className="search">
+        <div className="searchBar">
+          <img src="./search.png" alt="Search Icon" />
+          <input type="text" placeholder="Search" />
+        </div>
+        <img src="./plus.png" alt="Add Icon" className='add' />
+      </div>
+    </div>
+  );
 }
 
-export default ChatList
+export default ChatList;
--- src/components/list/userInfo/Userinfo.jsx
+++ src/components/list/userInfo/Userinfo.jsx
@@ -2,8 +2,18 @@ import "./userInfo.css";
 
 const Userinfo = () => {
   return (
-    <div className='userinfo'>Userinfo</div>
-  )
-}
+    <div className="userInfo">
+      <div className="user">
+        <img src="./avatar.png" alt="Avatar" />
+        <h2>Martin McCurley</h2>
+      </div>
+      <div className="icons">
+        <img src="./more.png" alt="More" />
+        <img src="./video.png" alt="Video" />
+        <img src="./edit.png" alt="Edit" />
+      </div>
+    </div>
+  );
+};
 
-export default Userinfo
+export default Userinfo;
