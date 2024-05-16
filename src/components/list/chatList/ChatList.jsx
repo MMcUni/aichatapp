5e44786876475c1345ef858a59d119f4d8690335
MMcUni <mmccur300@caledonian.ac.uk>
2024-05-16 15:49:54 +0100

    complete chatlist component design

--- src/components/list/chatList/ChatList.jsx
+++ src/components/list/chatList/ChatList.jsx
@@ -1,14 +1,56 @@
+import React, { useState } from 'react';
 import './chatList.css';
 
 const ChatList = () => {
+  const [addMode, setAddMode] = useState(false);
   return (
-    <div className='chatList'>
+    <div className="chatList">
       <div className="search">
         <div className="searchBar">
           <img src="./search.png" alt="Search Icon" />
           <input type="text" placeholder="Search" />
         </div>
-        <img src="./plus.png" alt="Add Icon" className='add' />
+        <img
+          src={addMode ? "./minus.png" : "./plus.png"}
+          alt="Add Icon"
+          className="add"
+          onClick={() => setAddMode((prev) => !prev)}
+        />
+      </div>
+      <div className="item">
+        <img src="./avatar.png" alt="" />
+        <div className="texts">
+          <span>Healthy Harry</span>
+          <p>Hello</p>
+        </div>
+      </div>
+      <div className="item">
+        <img src="./avatar.png" alt="" />
+        <div className="texts">
+          <span>Healthy Harry</span>
+          <p>Hello</p>
+        </div>
+      </div>
+      <div className="item">
+        <img src="./avatar.png" alt="" />
+        <div className="texts">
+          <span>Healthy Harry</span>
+          <p>Hello</p>
+        </div>
+      </div>
+      <div className="item">
+        <img src="./avatar.png" alt="" />
+        <div className="texts">
+          <span>Healthy Harry</span>
+          <p>Hello</p>
+        </div>
+      </div>
+      <div className="item">
+        <img src="./avatar.png" alt="" />
+        <div className="texts">
+          <span>Healthy Harry</span>
+          <p>Hello</p>
+        </div>
       </div>
     </div>
   );
