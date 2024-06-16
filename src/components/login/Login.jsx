56f235e01b4966fbd036e125adf4cff8cae95eda
MMcUni <mmccur300@caledonian.ac.uk>
2024-06-16 15:38:34 +0100

    user reg using firebase now functional

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,7 +1,8 @@
-import React, { useState } from "react";
-import { toast, ToastContainer } from "react-toastify";
-import 'react-toastify/dist/ReactToastify.css';
-import "./login.css";
+import { useState } from "react";
+import "./login.css";import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../lib/firebase";
 
 const Login = () => {
   const [avatar, setAvatar] = useState({
@@ -20,7 +21,22 @@ const Login = () => {
 
   const handleLogin = (e) => {
     e.preventDefault();
-    toast.warn("Hello!");
+  };
+
+  const handleRegister = async (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target)
+
+    const {username, email, password} = Object.fromEntries(formData);
+
+    try{
+
+      const res = await createUserWithEmailAndPassword(auth, email, password)
+
+    }catch(err){
+      console.log(err)
+      toast.error(err.message)
+    }
   };
 
   return (
@@ -37,17 +53,12 @@ const Login = () => {
       <div className="separator"></div>
       <div className="item">
         <h2>Create an Account</h2>
-        <form>
+        <form onSubmit={handleRegister}>
           <label htmlFor="file">
             <img src={avatar.url || "./avatar.png"} alt="Avatar" />
             Upload an image
           </label>
-          <input
-            type="file"
-            id="file"
-            style={{ display: "none" }}
-            onChange={handleAvatar}
-          />
+          <input type="file" id="file" style={{ display: "none" }} onChange={handleAvatar} />
           <input type="text" placeholder="Username" name="username" />
           <input type="text" placeholder="Email" name="email" />
           <input type="password" placeholder="Password" name="password" />
